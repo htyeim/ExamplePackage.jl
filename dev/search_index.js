c788a491a9495572a18abad25410e7f4faf9f22c
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-02-28 13:15:43 +0000

    build based on 7768528

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ExamplePackage.jl-1","page":"Home","title":"ExamplePackage.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [ExamplePackage]","category":"page"},{"location":"#test-1","page":"Home","title":"test","text":"","category":"section"}]
+}
